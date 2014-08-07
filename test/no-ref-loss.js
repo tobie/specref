ac1dc55f40566cfef7e6095061050af60dfbe5f1
Markus Lanthaler <mark_lanthaler@gmx.net>
2014-08-07 11:04:40 +0200

    Change no-ref-loss test as suggested by Tobie
    See https://github.com/tobie/specref/pull/113/files#r15924848

--- test/no-ref-loss.js
+++ test/no-ref-loss.js
@@ -2,14 +2,14 @@ var assert = require('assert'),
     fs = require('fs'),
     path = require('path');
 
-var current = Object.keys(require('../lib/bibref').all);
+var current = require('../lib/bibref');
 
 var previous = JSON.parse(fs.readFileSync(path.join(__dirname, 'ref-list.json'), 'utf8'));
 
 suite("Previous references are maintained", function() {
     previous.forEach(function(key) {
         test("Reference [[" + key + "]] is maintained.", function() {
-            assert.ok(current.indexOf(key.toUpperCase()) > -1);
+            assert.ok(current.get(key)[key]);
         });
     });
 });
