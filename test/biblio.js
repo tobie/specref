e170ab62d1a886d622e58aa5b966a05f37634780
Tobie <tobie.langel@gmail.com>
2015-02-13 22:38:31 +0100

    Add JSON schema of input JSON data.

--- test/biblio.js
+++ test/biblio.js
@@ -1,59 +1,32 @@
 var assert = require('assert');
 var bibref = require('../lib/bibref');
 var json = bibref.expandRefs(bibref.raw);
+var tv4 = require("tv4");
+var pointer = require("json-pointer");
 
 function wrap(id) {
     return '[[' + id + ']]';
 }
 
-var PROPS = ["authors", "etAl", "href", "title", "date", "deliveredBy", "rawDate", "status", "publisher", "isRetired", "hasErrata", "isSuperseded", "source", "unorderedAuthors", "updates", "obsoletes", "issn", "updatedBy", "obsoletedBy", "rfcNumber", "seeAlso", "id", "versionOf"];
-
-function testPropIsAString(obj, key, propName) {
-    test(wrap(key) + ' has a ' + propName + ' which isn\'t an empty string', function() {
-        assert.ok(typeof obj[propName] == "string")
-        assert.ok(obj[propName].length > 0, "is not the empty string")
+function displayErr(err, obj, output) {
+    output.push("* " + err.message.trim());
+    JSON.stringify(pointer.get(obj, err.dataPath), null, 4).split("\n").forEach(function(line) {
+        output.push("    " + line);
     });
 }
 
-function testObjOnlyContainsProps(obj, key, props) {
-    test(wrap(key) + ' has no other properties', function() {
-        Object.keys(obj).forEach(function(k) {
-            assert.ok(props.indexOf(k) > -1, k + ' is a prop');
-        })
-    });
-}
-
-function testEtAlIsTrueWhenPresent(obj, key) {
-    if ('etAl' in obj) {
-        test(wrap(key) + ' has an etAl property equal to true', function() {
-            assert.strictEqual(obj.etAl, true);
+function schemaMsg(err, obj) {
+    if (!err) return "";
+    var output = [""];
+    displayErr(err, obj, output);
+    if (err.subErrors) {
+        err.subErrors.forEach(function(s) {
+            displayErr(s, obj, output);
         });
     }
+    return output.join("\n");
 }
 
-function testAuthorsArray(obj, key) {
-    if ('authors' in obj) {
-        test(wrap(key) + ' has an authors array', function() {
-            assert.ok(obj.authors instanceof Array);
-            assert.ok(obj.authors.length > 0);
-            obj.authors.forEach(function(author) {
-                assert.ok(typeof author == "string");
-            })
-        });
-    }
-}
-
-function testDeliveredByArray(obj, key) {
-    if ('deliveredBy' in obj) {
-        test(wrap(key) + ' has a deliveredBy array', function() {
-            assert.ok(obj.deliveredBy instanceof Array);
-            assert.ok(obj.deliveredBy.length > 0);
-            obj.deliveredBy.forEach(function(url) {
-                assert.ok(typeof url == "string");
-            })
-        });
-    }
-}
 
 function testAliasOfPointsToRealObj(obj, key) {
     if ('aliasOf' in obj) {
@@ -79,58 +52,23 @@ function testForDuplicates(obj, key, dups) {
     });
 }
 
-suite('Reference', function() {
-    var dups = {};
+suite('Validate References', function() {
+    var r = tv4.validateResult(json, require("../schemas/raw-reference.json"));
+    assert.ok(r.valid, schemaMsg(r.error, json));
+});
 
+suite('Verify aliases resolve', function() {
+    var dups = {};
     Object.keys(json).forEach(function(key) {
         var obj = json[key];
-
         testForDuplicates(obj, key, dups);
-
         if (typeof obj == 'object') {
-            if (obj.aliasOf) {
-                testAliasOfPointsToRealObj(obj, key);
-                testObjOnlyContainsProps(obj, key, ['aliasOf']);
-            } else {
-                testAuthorsArray(obj, key);
-                testDeliveredByArray(obj, key);
-                testEtAlIsTrueWhenPresent(obj, key);
-
-                ['href', 'edDraft', 'date', 'status', 'publisher', 'title', 'versionOf', 'id'].forEach(function(prop) {
-                    if (prop in obj) {
-                        testPropIsAString(obj, key, prop);
-                    }
+            testAliasOfPointsToRealObj(obj, key);
+            if ('versions' in obj) {
+                var versions = obj.versions;
+                Object.keys(versions).forEach(function(k) {
+                    testAliasOfPointsToRealObj(versions[k], k);
                 });
-
-                testObjOnlyContainsProps(obj, key, PROPS.concat('versions', 'aliases', 'edDraft'));
-
-                if ('versions' in obj) {
-                    suite("versions obj of " + wrap(key), function() {
-                        var versions = obj.versions;
-
-                        test(wrap(key) + ' has a versions object', function() {
-                            assert.ok(typeof versions == "object");
-                        });
-
-                        Object.keys(versions).forEach(function(k) {
-                            var ver = versions[k];
-                            if ('aliasOf' in ver) {
-                                testAliasOfPointsToRealObj(ver, k);
-                                testObjOnlyContainsProps(ver, k, ['aliasOf']);
-                            } else {
-                                ['href', 'title', 'date', 'status', 'publisher', 'versionOf', 'id'].forEach(function(prop) {
-                                    if (prop in ver) {
-                                        testPropIsAString(ver, k, prop);
-                                    }
-                                });
-                                testAuthorsArray(ver, k);
-                                testDeliveredByArray(ver, k);
-                                testEtAlIsTrueWhenPresent(ver, k);
-                                testObjOnlyContainsProps(ver, k, PROPS);
-                            }
-                        });
-                    });
-                }
             }
         } else {
             test('is a string', function() {
