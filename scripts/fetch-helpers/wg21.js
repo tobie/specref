f7f962deb2715b3a921a36b07168096799e7cfc3
Tobie Langel <tobie@codespeaks.com>
2017-02-23 10:13:05 +0100

    cleanup wg21 import script

--- scripts/fetch-helpers/wg21.js
+++ scripts/fetch-helpers/wg21.js
@@ -13,6 +13,12 @@ var MONTHS = {
     dec: "December"
 };
 
+var RAW_DATE = /^\d\d\d\d-(0\d|1[0-2])-[0-2]\d$/;
+var MONTH_SHORTNAME = /\b[a-z]{3,}\b/i;
+var UNDESIRED_CHAR = /[^0-9a-zA-Z\s]/g;
+var LEADING_ZERO = /^0/;
+var EXTRA_WS = /\s+/g;
+
 module.exports = function(id, ref) {
     var output = {
         title: ref.title,
@@ -25,12 +31,12 @@ module.exports = function(id, ref) {
         output.authors = [ref.submitter];
     }
     if (ref.date && ref.date != "unknown" ) {
-        if (/^\d\d\d\d-(0\d|1[0-2])-[0-2]\d$/.test(ref.date)) {
+        if (RAW_DATE.test(ref.date)) {
             output.rawDate = ref.date;
         } else {
-            output.date = ref.date.replace(/\b[a-z]{3,}\b/i, function(mmm) {
+            output.date = ref.date.replace(MONTH_SHORTNAME, function(mmm) {
                 return MONTHS[mmm.toLowerCase()] || mmm;
-            }).replace(/[^0-9a-zA-Z\s]/g, "").replace(/^0/, "").replace(/\s+/g, " ");
+            }).replace(UNDESIRED_CHAR, "").replace(LEADING_ZERO, "").replace(EXTRA_WS, " ");
         }
     }
     
