4e73a830a386b3484e1ff43b11e533f3f3c6798f
Tobie Langel <tobie@unlockopen.com>
2019-04-17 16:48:06 +0800

    Update scripts/fetch-helpers/scte.js
    Fixes #528 (at least in SCTE scope)
    
    Co-Authored-By: sandersaares <sandersaares@users.noreply.github.com>

--- scripts/fetch-helpers/scte.js
+++ scripts/fetch-helpers/scte.js
@@ -1,6 +1,6 @@
 module.exports = function(id, ref) {
     return {
-        href: ref.href,
+        href: encodeURI(ref.href),
         title: ref.title,
         publisher: ref.publisher,
         rawDate: ref.rawDate
